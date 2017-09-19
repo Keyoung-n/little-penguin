ea355f2631ca9283d1852d0565df4804de0eaeea
Keyoung-n <keang.nage.3@hotmail.com>
2017-09-19 18:23:15 +0200

    added gitignore

--- ex01/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/little-penguin/ex01/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /root/little-penguin/ex01/hello.ko /root/little-penguin/ex01/hello.o /root/little-penguin/ex01/hello.mod.o ;  true
--- ex01/.tmp_versions/hello.mod
+++
@@ -1,3 +0,0 @@
-/root/little-penguin/ex01/hello.ko
-/root/little-penguin/ex01/hello.o
-
