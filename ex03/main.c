4ad81221e7c4b2c9121dff828bba71d014183d07
Keyoung-n <keang.nage.3@hotmail.com>
2017-09-20 08:58:48 +0200

    finished ex03

--- ex03/main.c
+++ ex03/main.c
@@ -2,27 +2,34 @@
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/slab.h>
+
+/*
+ * This function sleeps for set amount of time, if long enough it tells the user
+ * space about it.
+ */
 int do_work(int *my_int, int retval)
 {
 	int counter;
 	int tmp = *my_int;
 	int product;
-	for (counter = 0; counter < my_int; ++counter)
+	for (counter = 0; counter < *my_int; ++counter)
 		udelay(10);
 	if (tmp < 10)
-		/* That was a long sleep, tell userspace about it */
 		pr_info("We slept a long time!");
-	product = couter * tmp;
-	return product;
+	product = counter * tmp;
+	return 0;
 }
+
 int my_init(void)
 {
-	int tmp = 10;
-	tmp = do_work(&tmp, tmp);
-	return x;
+	int ret = 10;
+	ret = do_work(&ret, ret);
+	return ret;
 }
+
 void my_exit(void)
 {
 }
+
 module_init(my_init);
 module_exit(my_exit);
