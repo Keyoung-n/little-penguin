c67a560395a319d92a2d225a4eb7101cffde4d27
Keyoung-n <keang.nage.3@hotmail.com>
2017-09-22 13:38:57 +0200

    ex05 moving to mac

--- ex05/main.c
+++ ex05/main.c
@@ -4,21 +4,22 @@
 #include <linux/miscdevice.h>
 #include <linux/init.h>
 #include <linux/string.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Knage");
 MODULE_DESCRIPTION("Fortytwo misc device driver.");
 
 static int misc_open(struct inode *inode, struct file *file) {
-  return 0;
+	return 0;
 }
 
 static int misc_close(struct inode *inodep, struct file *filp) {
-  return 0;
+	return 0;
 }
 
 static ssize_t misc_write(struct file *file, const char __user *buf,
-  size_t len, loff_t *pppos)
+			  size_t len, loff_t *pppos)
 {
 	printk(KERN_NOTICE "buf == %s.\n", buf);
 	if (strncmp("knage", buf, 5) == 0) {
@@ -29,13 +30,14 @@ static ssize_t misc_write(struct file *file, const char __user *buf,
 
 static ssize_t misc_read(struct file *filep, char *buf, size_t len, loff_t *offset)
 {
-   if (copy_to_user(buf, "knage", 5)){
-      printk(KERN_INFO "sent message to user");
-      return (5);
-   }
-   else {
-      return -EFAULT;
-   }
+	int error_count = 0;
+	memset(buf, 0, 10);
+	char *login = "knage";
+	error_count = copy_to_user(buf, login, 6);
+	if (error_count == 0) {
+		return -EFAULT;
+	}
+	return (5);
 }
 
 static const struct file_operations misc_fops = {
@@ -48,21 +50,21 @@ static const struct file_operations misc_fops = {
 };
 
 struct miscdevice misc_device = {
-  .minor = MISC_DYNAMIC_MINOR,
-  .name  = "fortytwo",
-  .fops  = &misc_fops
+	.minor = MISC_DYNAMIC_MINOR,
+	.name  = "fortytwo",
+	.fops  = &misc_fops
 };
 
 static int __init misc_init(void)
 {
 	if (misc_register(&misc_device))
 		printk(KERN_ERR "Unable register misc device");
-  return 0;
+	return 0;
 }
 module_init(misc_init);
 
 static void __exit misc_exit(void)
 {
-      misc_deregister(&misc_device);
+	misc_deregister(&misc_device);
 }
 module_exit(misc_exit);
