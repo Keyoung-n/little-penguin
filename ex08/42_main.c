591069c7b91cbc340050a005ebeb85298120fe58
Keang NAGE <knage@e5r8p9.wethinkcode.co.za>
2017-09-26 10:17:30 +0200

    fixed registering module

--- ex08/42_main.c
+++ ex08/42_main.c
@@ -4,7 +4,7 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
-// Dont have a license, LOL
+
 MODULE_LICENSE("LICENSE");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
@@ -28,12 +28,13 @@ char str[PAGE_SIZE];
 char *tmp;
 
 static int __init myfd_init(void) {
-	int retval;
-	retval = misc_register(&(*(&(myfd_device))));
-	return 1;
+  if (misc_register(&myfd_device))
+    printk(KERN_ERR "Unable register misc device");
+	return 0;
 }
 
 static void __exit myfd_cleanup(void) {
+  misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
@@ -53,7 +54,7 @@ ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs) {
 	ssize_t res;
 	res = 0;
-	res = simple_write_to_buffer(str, size, offs, user, size) + 1; 
+	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
 	str[size + 1] = 0x0;
 	return res;
 }
