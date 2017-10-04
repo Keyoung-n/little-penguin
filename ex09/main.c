b2d1a9f2106f59c8a5c36d850b0840eac834dc54
Keyoung-n <keang.nage.3@hotmail.com>
2017-10-04 15:09:59 +0200

    need lisence

--- ex09/main.c
+++ ex09/main.c
@@ -1,40 +1,35 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 
 struct proc_dir_entry *File;
+int temp = 0;
 #define file_name "mymounts"
 
-int read_proc(struct file *filp, char *buf, size_t count, loff_t *offp )
+ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp )
 {
-  if(count > temp)
-    count = temp;
-  temp -= count;
-  copy_to_user(buf, "hello world", count);
-  if(count == 0)
-    temp = len;
-
-  return count;
+	int ret = 0;
+	ret = copy_to_user(buf, "hello world", count);
+	return 0;
 }
 
-return count;
-}
 
 struct file_operations proc_fops = {
-  read:  read_proc
+	.read = read_proc
 };
 
-int init_module()
+int proc_init(void)
 {
 	proc_create(file_name, 0, NULL, &proc_fops);
 
 	return 0;
 }
 
-void cleanup_module()
+void proc_cleanup(void)
 {
 	remove_proc_entry(file_name, NULL);
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(proc_init);
+module_exit(proc_cleanup);
