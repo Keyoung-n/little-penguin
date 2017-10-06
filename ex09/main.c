ffeaa463ba52fa828b84c5f0ed00511b2ee5fdba
Keyoung-n <keang.nage.3@hotmail.com>
2017-10-06 12:51:23 +0200

    finished little pen

--- ex09/main.c
+++ ex09/main.c
@@ -1,30 +1,53 @@
-#include <linux/module.h>
+
+#include <linux/nsproxy.h>
+#include <linux/fs.h>
 #include <linux/kernel.h>
-#include <linux/proc_fs.h>
+#include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/types.h>
 #include <linux/uaccess.h>
+#include <linux/dcache.h>
+#include <linux/init.h>
+#include <linux/path.h>
+#include <linux/namei.h>
+#include <linux/string.h>
+#include <linux/seq_file.h>
+#include <linux/mnt_namespace.h>
+#include <linux/fs_struct.h>
+#include <linux/proc_fs.h>
+#include <linux/mount.h>
+#include <linux/wait.h>
+#include <linux/fs_pin.h>
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Knage");
-MODULE_DESCRIPTION("Fortytwo module.");
-
-struct proc_dir_entry *File;
-#define file_name "mymounts"
+char *file_name = "mymounts";
 
 ssize_t read_proc(struct file *filp, char *buf, size_t len, loff_t *offp )
 {
-  char str[] = "hello world!\n";
-  return simple_read_from_buffer(buf, len, offset, str, strlen(str));
+	struct dentry *curdentry;
+	printk("root      %s", current->fs->root.mnt->mnt_root->d_name.name);
+	list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child)
+	{
+		if (curdentry->d_flags & DCACHE_MOUNTED)
+			printk("%s      /%s", curdentry->d_name.name, curdentry->d_name.name);
+	}
+	return 0;
 }
 
+static int open_proc(struct inode *inode, struct file *file)
+{
+		return 0;
+}
 
 struct file_operations proc_fops = {
-	.read = read_proc
+	.read = read_proc,
+	.open = open_proc,
 };
 
 int proc_init(void)
 {
 	proc_create(file_name, 0, NULL, &proc_fops);
-
+	
 	return 0;
 }
 
@@ -35,3 +58,8 @@ void proc_cleanup(void)
 
 module_init(proc_init);
 module_exit(proc_cleanup);
+
+MODULE_DESCRIPTION("Misc device driver");
+MODULE_AUTHOR("Kcowle");
+MODULE_LICENSE("GPL");
+
